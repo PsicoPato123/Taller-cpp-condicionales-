a7ed3e1064f202b2e8bb0fc9753a6971772fb09f
PsicoPato123 <lu.vg0603@gmail.com>
2026-03-02 09:49:17 -0500

    Refactor permission system for patipermisos
    Updated permission system to use 'patipermisos' and adjusted input range.

--- ejercicio-3.cpp
+++ ejercicio-3.cpp
@@ -1,35 +1,38 @@
+#include <iostream>
 int main() {
-    const int read  = 4; 
-    const int write = 2; 
-    const int execute = 1; 
-
+    const int read  = 4; //equivale a 100
+    const int write = 2; //equivale a 010
+    const int execute = 1; //equivale a 001
     int Permiso;
-
-    std::cout << "Sistema de Permisos Bitmask" << endl;
-    std::cout << "Ingrese un valor de permisos (0-7): ";
+    std::cout << "Sistema de Permisos Patito\n";
+    std::cout << "Ingrese un valor de patipermisos de 0 a 5: ";
     std::cin >> Permiso;
-    std::cout << "\nAnalizando permisos para el valor " << Permiso << ":" << endl;
+    std::cout << "\nAnalizando patipermisos para el valor " << Permiso << ":";
+
+    if (Permiso < 0 || Permiso > 5) {
+    std::cout << "Valor invalido\n. Debe estar entre 0 y 5.\n";
+    return 0;
 
     if (Permiso & read) {
-       std:: cout << "[X] LEER (4)" << endl;
+       std:: cout << "[X] LEER (4)";
     } else {
-       std:: cout << "[ ] LEER" << endl;
+       std:: cout << "[ ] LEER";
     }
 
     if (Permiso & write) {
-       std:: cout << "[X] ESCRIBIR (2)" << endl;
+       std:: cout << "[X] ESCRIBIR (2)";
     } else {
-        std::cout << "[ ] ESCRIBIR" << endl;
+        std::cout << "[ ] ESCRIBIR";
     }
 
     if (Permiso & execute) {
-        std::cout<< "[X] EJECUTAR (1)" << endl;
+        std::cout<< "[X] EJECUTAR (1)";
     } else {
-        std::cout << "[ ] EJECUTAR" << endl;
+        std::cout << "[ ] EJECUTAR" ;
     }
     if (Permiso == 0) {
-       std::cout << "El usuario no tiene ningun permiso." << endl;
+       std::cout << "El usuario no tiene ningun patipermiso\n " ;
     }
-
     return 0;
 }
+
